872cb858562b3ab63fde9fcbc871dc4b210249a5
Alessandro Pedano <alessandro.pedano@transsmart.com>
2020-02-16 15:44:40 +0100

    Added sleuth for distributed tracing

--- src/main/java/apedano/microservices/netflixzuulapigatewayserver/NetflixZuulApiGatewayServerApplication.java
+++ src/main/java/apedano/microservices/netflixzuulapigatewayserver/NetflixZuulApiGatewayServerApplication.java
@@ -1,17 +1,27 @@
 package apedano.microservices.netflixzuulapigatewayserver;
 
+import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableZuulProxy
 @EnableDiscoveryClient //Eureka
 public class NetflixZuulApiGatewayServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NetflixZuulApiGatewayServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(NetflixZuulApiGatewayServerApplication.class, args);
+    }
+
+    /**
+     * Bean used by Sleuth to specify the requests to trace
+     */
+    @Bean
+    public Sampler defautSampler() {
+        return Sampler.ALWAYS_SAMPLE;
+    }
 
 }
